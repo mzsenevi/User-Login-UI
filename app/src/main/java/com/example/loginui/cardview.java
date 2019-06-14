0c541fc44620fbffc7520d1861901627c7cdc381
mzsenevi <maneesha.seneviratne17@gmail.com>
2019-06-14 19:56:20 +0530

    LoginUI

---
+++ app/src/main/java/com/example/loginui/cardview.java
@@ -0,0 +1,13 @@
+package com.example.loginui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cardview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+    }
+}
